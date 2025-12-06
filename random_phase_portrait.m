c1797bd18e33231428a9d815593b26416f63a282
mateos-yago <yagomateos@gmail.com>
2025-12-05 20:08:50 -0500

    last update

--- random_phase_portrait.m
+++ random_phase_portrait.m
@@ -1,9 +1,9 @@
 
 % random initial conditions:
 
-rng(138)
+rng(139)
 
-n = 16; % number of vectors
+n = 32; % number of vectors
 K = 16*n; % number of arms
 dim = 2*n;
 
@@ -16,24 +16,9 @@ mu = 1./(1:(2*d+1));
 
 M = polyflow.mse_build_model(f, g, mu);
 
-figure(1);
 
-polyflow.mse_phaseportrait(M, []);  % this draws the log L, backbone, skeleton, etc.
 
 opts = struct();
+opts.eta = 0.0005;
 
-%visualizer for 2D case
-%polyflow.mse_plot_midpoint_trajectories(M, Y0, opts);
-
-
-opts.eta = 0.0005; opts.n_steps = 1e4;
-loss_table = polyflow.mse_plot_ucb_trajectories_multiple_points(M, Y0, 50000, opts);
-
-%opts.eta = 1e-4; opts.n_steps = 10/opts.eta;
-%polyflow.mse_plot_sgd_trajectories(M, Y0, opts);
-
-%opts.eta = 1e-2; opts.n_steps = 10/opts.eta;
-%polyflow.mse_plot_adamw_trajectories(M, Y0, opts);
-
-%opts.eta = 1e-3; opts.n_steps = 10/opts.eta;
-%polyflow.mse_plot_muon_trajectories(M, Y0, opts);
+polyflow.mse_plot_ucb_trajectories_multiple_points(M, Y0, n*10000, opts);
